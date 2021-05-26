e5f5dce0e24c4bd1232a972b054c570a7b410bf0
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2021-05-26 17:33:46 +0200

    Create create_users.sh
    Crear Usuarios inline

---
+++ create_users.sh
@@ -0,0 +1,5 @@
+#NeWare 2021 ~ Grup A
+contrasenyaencriptada="$(mkpasswd --method=sha-512 $2)"
+useradd -m -p $contrasenyaencriptada -s /bin/bash $1
+usermod -g $3 $1
+echo "S'ha Creat l'Usuari $1 amb Contrasenya $2 i amb Grup $3"
